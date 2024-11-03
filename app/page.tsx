59f0f4fb84c284f9350f5d4f01bcc1fc4493a60d
Martin N <na@na.no>
2024-11-03 23:19:54 +0100

    changes for bug reproduction

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
 /** Add your relevant code here for the issue to reproduce */
+
+import classes from './page.module.css'
+import classesNoBom from './page-no-bom.module.css'
+
 export default function Home() {
-  return null;
+
+  return <>
+  <div className={classes.red}>Using css module with  UTF8-BOM </div>
+  <div className={classesNoBom.red}>Using css module with UTF8</div>
+  </>
 }
